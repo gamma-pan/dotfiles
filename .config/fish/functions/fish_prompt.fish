c3f595a8f9ecf9852859f1a64ef4f5d67e093120
gamma_pan <yiorgospanag@gmail.com>
2021-06-28 20:00:31 +0300

    minimal changes in fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -8,7 +8,7 @@ function fish_prompt
 	set_color blue
 	echo -n (prompt_pwd)	
 	set_color white
-	echo -n ') $ '
+	echo -n ') > '
 	set_color normal
 
 end
--- .config/fish/functions/run.fish
+++
@@ -1,3 +0,0 @@
-function run -d "It launches a, usually gui application detached from parent terminal"
-	;
-end
